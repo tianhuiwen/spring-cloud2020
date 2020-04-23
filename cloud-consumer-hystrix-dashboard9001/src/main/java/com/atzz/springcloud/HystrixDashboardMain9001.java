f2347e60511fdd5e0829766364f171e069226d02
tianhuiwen <tianhuiwensir@163.com>
2020-04-23 12:00:10 +0800

    新增加 HystrixDashboard 工程

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atzz/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.atzz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author tianhuiwen
+ * @date 2020/4/23 11:58
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
