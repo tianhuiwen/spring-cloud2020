2795aa9d4eca773444e7ebca48fc39826cbd0504
tianhuiwen <tianhuiwensir@163.com>
2020-04-24 11:27:50 +0800

    新增加 gateway 工程

---
+++ cloud-gateway-gateway9527/src/main/java/com/atzz/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.atzz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author tianhuiwen
+ * @date 2020/4/24 11:25
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
