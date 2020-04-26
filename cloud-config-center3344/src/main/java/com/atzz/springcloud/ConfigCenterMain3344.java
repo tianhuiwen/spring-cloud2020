c7212bf0f5ca9a339eb87fd4751c51154e9cb03f
tianhuiwen <tianhuiwensir@163.com>
2020-04-26 16:02:04 +0800

    新增服务配置中心工程

---
+++ cloud-config-center3344/src/main/java/com/atzz/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.atzz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author tianhuiwen
+ * @date 2020/4/26 16:00
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
