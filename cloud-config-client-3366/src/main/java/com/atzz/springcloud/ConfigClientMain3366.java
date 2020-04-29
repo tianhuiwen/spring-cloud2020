64e7adbbe39a111e2c4f520c541240a1cdbe13b5
tianhuiwen <tianhuiwensir@163.com>
2020-04-29 16:49:15 +0800

    新增 cloud-config-client3366 工程

---
+++ cloud-config-client-3366/src/main/java/com/atzz/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.atzz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author tianhuiwen
+ * @date 2020/4/29 15:35
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
