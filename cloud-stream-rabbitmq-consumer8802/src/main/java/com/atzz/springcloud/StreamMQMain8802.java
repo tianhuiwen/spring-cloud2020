0c333f448eb1e220889dd0e3b5497cc5c676f73d
tianhuiwen <tianhuiwensir@163.com>
2020-06-09 22:05:24 +0800

    新增 cloud-stream-rabbitmq-consumer8802 工程

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atzz/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.atzz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tianhuiwen
+ * @date 2020/6/9 22:03
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
